8f0c1d9c8a99422ef2903b93a534732ef7818df1
Mersmith <mersmith14@gmail.com>
2024-06-30 11:55:32 -0500

    seeder inventario

--- database/seeders/GuiaEntradaDirectoSeeder.php
+++ database/seeders/GuiaEntradaDirectoSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\GuiaEntradaDirecto;
 use App\Models\GuiaEntradaDirectoDetalle;
+use App\Models\Inventario;
 use App\Models\Variacion;
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +15,7 @@ class GuiaEntradaDirectoSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
         GuiaEntradaDirecto::factory(10)->create()->each(function ($guiaEntradaDirecto) {
             // Obtener todas las variaciones y mezclarlas
@@ -24,10 +26,30 @@ class GuiaEntradaDirectoSeeder extends Seeder
 
             // Crear detalles únicos para cada GuiaEntradaDirecto
             for ($i = 0; $i < $detalleCount; $i++) {
-                GuiaEntradaDirectoDetalle::factory()->create([
+                $detalle = GuiaEntradaDirectoDetalle::factory()->create([
                     'guia_entrada_directo_id' => $guiaEntradaDirecto->id,
                     'variacion_id' => $variaciones[$i]->id,
                 ]);
+
+                // Si la guía de entrada directo está aprobada, actualizar inventario
+                if ($guiaEntradaDirecto->estado == 'Aprobado') {
+                    $inventario = Inventario::updateOrCreate(
+                        [
+                            'almacen_id' => $guiaEntradaDirecto->almacen_id,
+                            'variacion_id' => $detalle->variacion_id,
+                        ],
+                        [
+                            'stock' => DB::raw('stock + ' . $detalle->stock),
+                            'stock_minimo' => DB::raw('stock_minimo + ' . $detalle->stock_minimo),
+                        ]
+                    );
+                }
+            }
+
+            // Actualizar el estado de completado en la guía de entrada directo
+            if ($guiaEntradaDirecto->estado == 'Aprobado') {
+                $guiaEntradaDirecto->completado = true;
+                $guiaEntradaDirecto->save();
             }
         });
     }
