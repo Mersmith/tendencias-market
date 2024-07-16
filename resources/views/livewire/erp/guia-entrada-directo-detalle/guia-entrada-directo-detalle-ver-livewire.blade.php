67ce85ccca05a28453cb4ce8f522a31f5194ecce
Mersmith <mersmith14@gmail.com>
2024-07-16 11:22:54 -0500

    detalle guia

--- resources/views/livewire/erp/guia-entrada-directo-detalle/guia-entrada-directo-detalle-ver-livewire.blade.php
+++ resources/views/livewire/erp/guia-entrada-directo-detalle/guia-entrada-directo-detalle-ver-livewire.blade.php
@@ -19,27 +19,33 @@
         </div>
     </div>
 
+    <!--FORMULARIO-->
     <div class="formulario">
         <div class="g_fila">
             <div class="g_columna_8">
                 <div class="g_panel">
+                    <!--TITULO-->
                     <h4 class="g_panel_titulo">General</h4>
 
+                    <!--ID ENTRADA-->
                     <div class="g_margin_bottom_20">
-                        <label for="guia_id">ID </label>
+                        <label for="guia_id">ID Guia</label>
                         <input type="text" id="guia_id" name="guia_id" value="{{ $guia->id }}" disabled>
                     </div>
 
+                    <!--DESCRIPCION-->
                     <div class="g_margin_bottom_20">
                         <label for="descripcion">Descripción</label>
                         <textarea id="descripcion" name="descripcion" disabled>{{ $guia->descripcion }}</textarea>
                     </div>
 
+                    <!--OBSERVACION-->
                     <div class="g_margin_bottom_20">
                         <label for="observacion">Observación</label>
                         <textarea id="observacion" name="observacion" disabled>{{ $guia->observacion }}</textarea>
                     </div>
 
+                    <!--FECHA ENTRADA-->
                     <div class="g_margin_bottom_20">
                         <label for="fecha_entrada">Fecha entrada</label>
                         <input type="date" id="fecha_entrada" name="fecha_entrada" value="{{ $guia->fecha_entrada }}"
@@ -50,24 +56,51 @@
 
             <div class="g_columna_4">
                 <div class="g_panel">
+                    <!--TITULO-->
                     <h4 class="g_panel_titulo">Estado</h4>
 
-                    <input type="text" id="guia_id" name="guia_id" value="{{ $guia->estado }}" disabled>
+                    <!--ESTADO-->
+                    <input type="text" id="estado" name="estado" value="{{ $guia->estado }}" disabled>
                 </div>
 
                 <div class="g_panel">
+                    <!--TITULO-->
                     <h4 class="g_panel_titulo">Completado</h4>
 
-                    <input type="text" id="guia_id" name="guia_id"
+                    <!--COMPLETADO-->
+                    <input type="text" id="completado" name="completado"
                         value="{{ $guia->completado == 1 ? 'Completado' : 'Falta' }}" disabled>
+                </div>
+
+                <div class="g_panel">
+                    <!--TITULO-->
+                    <h4 class="g_panel_titulo">Detalle</h4>
+
+                    <!--SEDE-->
+                    <div class="g_margin_bottom_20">
+                        <label for="fecha_entrada">Sede</label>
+                        <input type="text" id="sede_id" name="sede_id" value="{{ $guia->sede->nombre }}" disabled>
+                    </div>
+
+                    <!--ALMACEN-->
+                    <div class="g_margin_bottom_20">
+                        <label for="fecha_entrada">Almacén</label>
+                        <input type="text" id="almacen_id" name="almacen_id" value="{{ $guia->almacen->nombre }}"
+                            disabled>
+                    </div>
+
+                    <!--SERIE CORRELATIVO-->
+                    <div class="g_margin_bottom_20">
+                        <label for="fecha_entrada">Serie</label>
+                        <input type="text" id="serie_id" name="serie_id" value="{{ $guia->serie }} - {{ $guia->correlativo }}" disabled>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <!--CONTENEDOR PÁGINA ADMINISTRADOR-->
+    <!--TABLA-->
     <div class="g_panel">
-        <!--TABLA-->
         @if ($detalle->count())
             <!--TABLA CABECERA-->
             <div class="tabla_cabecera">
@@ -81,14 +114,14 @@
             <!--TABLA CONTENIDO-->
             <div class="tabla_contenido">
                 <div class="contenedor_tabla">
-                    <!--TABLA-->
                     <table class="tabla">
                         <thead>
                             <tr>
                                 <th>Nº</th>
-                                <th>Nombre producto</th>
-                                <th>Nombre color</th>
-                                <th>Nombre talla</th>
+                                <th>ID Variacion</th>
+                                <th>Producto</th>
+                                <th>Talla</th>
+                                <th>Color</th>
                                 <th>Stock</th>
                                 <th>Stock mínimo</th>
                             </tr>
@@ -97,9 +130,11 @@
                             @foreach ($detalle as $item)
                                 <tr>
                                     <td class="g_resaltar">{{ $loop->iteration }}</td>
-                                    <td class="g_resaltar">{{ $item->variacion->producto->nombre ?? '-' }}</td>
-                                    <td class="g_resaltar">{{ $item->variacion->color->nombre ?? '-' }}</td>
+                                    <td class="g_resaltar">{{ $item->variacion->id }}</td>
+                                    <td class="g_resaltar">ID: {{ $item->variacion->producto->id }} -
+                                        {{ $item->variacion->producto->nombre }}</td>
                                     <td class="g_resaltar">{{ $item->variacion->talla->nombre ?? '-' }}</td>
+                                    <td class="g_resaltar">{{ $item->variacion->color->nombre ?? '-' }}</td>
                                     <td class="g_inferior g_resumir">{{ $item->stock }}</td>
                                     <td class="g_inferior g_resumir">{{ $item->stock_minimo }}</td>
                                 </tr>
--- routes/erp.php
+++ routes/erp.php
@@ -142,10 +142,10 @@ Route::get('/inventario-variacion-lista-precio', InventarioVariacionListaPrecioT
 Route::get('/variacion-lista-precio', VariacionListaPrecioTodasLivewire::class)->name('variacion-lista-precio.vista.todas');//ok
 
 Route::get('/guia-entrada-directo', GuiaEntradaDirectoTodasLivewire::class)->name('guia-entrada-directo.vista.todas');//ok
-Route::get('/guia-entrada-directo/crear', GuiaEntradaDirectoCrearLivewire::class)->name('guia-entrada-directo.vista.crear');//
-Route::get('/guia-entrada-directo/{id}/detalle', GuiaEntradaDirectoDetalleVerLivewire::class)->name('guia-entrada-directo-detalle.vista.ver');
+Route::get('/guia-entrada-directo/crear', GuiaEntradaDirectoCrearLivewire::class)->name('guia-entrada-directo.vista.crear');//ok
+Route::get('/guia-entrada-directo/{id}/detalle', GuiaEntradaDirectoDetalleVerLivewire::class)->name('guia-entrada-directo-detalle.vista.ver');//ok
 
-Route::get('/transferencia-almacen', TransferenciaAlmacenTodasLivewire::class)->name('transferencia-almacen.vista.todas');
+Route::get('/transferencia-almacen', TransferenciaAlmacenTodasLivewire::class)->name('transferencia-almacen.vista.todas');//
 Route::get('/transferencia-almacen/crear', TransferenciaAlmacenCrearLivewire::class)->name('transferencia-almacen.vista.crear');
 Route::get('/transferencia-almacen/{id}/detalle', TransferenciaAlmacenDetalleVerLivewire::class)->name('transferencia-almacen-detalle.vista.ver');
 
