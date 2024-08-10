ddc4fcdf87b892dda805089ad6c4ed193ed94917
Mersmith <mersmith14@gmail.com>
2024-08-10 12:45:03 -0500

    avance

--- resources/views/ecommerce/partials/carrusel.blade.php
+++ resources/views/ecommerce/partials/carrusel.blade.php
@@ -1,47 +1,69 @@
 @if (!empty($p_elementos))
-    <div x-data="dataSliderProductos({{ count($p_elementos) }})" x-init="initSliderProductos()" class="partials_contenedor_carrusel">
 
-        <!-- SLIDER -->
-        <div x-ref="slider" class="contenedor_slide">
-            @foreach ($p_elementos as $index => $slide)
-                <div class="item_slide">
-                    <div class="contenedor_imagen">
-                        <img src="{{ $slide->url }}" alt="" class="imagen_computadora" />
+    <div x-data="dataCarrusel({{ count($p_elementos) }})" x-init="initSliderProductos()" class="partials_contenedor_carrusel">
+        <div class="partials_contenedor_carrusel_cabecera">
+            @foreach ($p_elementos as $index => $imagen)
+                <div>
+                    <div
+                        x-bind:class="posicionImagenActual === @json($index) ? 'imagen_activo' : 'imagen_oculto'">
+                        <img src="{{ $imagen->url }}" alt="" />
                     </div>
                 </div>
             @endforeach
-        </div>
-
-        <!-- CONTROL BOTONES -->
-        <div class="control_botones" x-show="totalPaginas > 1">
-            <button @click="botonRetroceder()" :disabled="paginaActual === 1" class="boton_retroceder">
+            <button @click="botonRetrocederImagen">
                 <i class="fa-solid fa-chevron-left"></i>
             </button>
-            <button @click="botonSiguiente()" :disabled="paginaActual + itemsPorPagina > totalElementos"
-                class="boton_siguiente">
+            <button @click="botonSiguienteImagen">
                 <i class="fa-solid fa-chevron-right"></i>
             </button>
         </div>
 
-        <!-- PAGINACION BOTONES -->
-        <div class="paginacion_botones" x-show="totalPaginas > 1">
-            <template x-for="itemPagina in totalPaginas" :key="itemPagina">
-                <button @click="setPaginaActual(itemPagina)"
-                    :class="{ 'boton_activo': paginaActual === (itemPagina - 1) * itemsPorPagina + 1 }"></button>
-            </template>
+        <div class="partials_contenedor_carrusel_pie">
+
+            <!-- SLIDER -->
+            <div x-ref="slider" class="contenedor_slide">
+                @foreach ($p_elementos as $index => $imagen)
+                    <div class="item_slide">
+                        <div class="contenedor_imagen" @click="setPosicionImagenActual({{ $index }})">
+                            <img src="{{ $imagen->url }}" alt=""
+                                :class="{ 'imagen_elegida': posicionImagenActual === {{ $index }} }" />
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <!-- CONTROL BOTONES -->
+            <div class="control_botones" x-show="totalPaginas > 1">
+                <button @click="botonRetroceder()" :disabled="paginaActual === 1" class="boton_retroceder">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </button>
+                <button @click="botonSiguiente()" :disabled="paginaActual + itemsPorPagina > totalElementos"
+                    class="boton_siguiente">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+
+            <!-- PAGINACION BOTONES -->
+            <div class="paginacion_botones" x-show="totalPaginas > 1">
+                <template x-for="itemPagina in totalPaginas" :key="itemPagina">
+                    <button @click="setPaginaActual(itemPagina)"
+                        :class="{ 'boton_activo': paginaActual === (itemPagina - 1) * itemsPorPagina + 1 }"></button>
+                </template>
+            </div>
         </div>
     </div>
 
     <script>
-        function dataSliderProductos(totalProductos) {
+        function dataCarrusel(totalImagenes) {
             return {
-                totalElementos: totalProductos,
+                posicionImagenActual: 0,
+                totalElementos: totalImagenes,
                 cantidadElementosComputadora: 6,
                 cantidadElementosTablet: 4,
                 cantidadElementosMovil: 5,
                 itemsPorPagina: 6,
                 paginaActual: 1,
-                totalPaginas: Math.ceil(totalProductos / 6),
+                totalPaginas: Math.ceil(totalImagenes / 6),
 
                 initSliderProductos() {
                     this.anchoPantalla();
@@ -59,6 +81,18 @@
                     }
                     this.totalPaginas = Math.ceil(this.totalElementos / this.itemsPorPagina);
                     this.scrollPaginaActual();
+                },
+
+                setPosicionImagenActual(index) {
+                    this.posicionImagenActual = index;
+                },
+
+                botonRetrocederImagen() {
+                    this.posicionImagenActual = this.posicionImagenActual - 1
+                },
+
+                botonSiguienteImagen() {
+                    this.posicionImagenActual = this.posicionImagenActual + 1
                 },
 
                 botonRetroceder() {
@@ -85,7 +119,8 @@
                             behavior: 'smooth'
                         });
                     }
-                }
+                },
+
             };
         }
     </script>
