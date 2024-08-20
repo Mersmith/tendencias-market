f49abf21a505d28b61724f8b99fb33f4ab724013
Mersmith <mersmith14@gmail.com>
2024-08-20 14:22:58 -0500

    temporizador

--- resources/views/ecommerce/partials/temporizador.blade.php
+++ resources/views/ecommerce/partials/temporizador.blade.php
@@ -4,8 +4,6 @@
         <div class="contenedor_fecha_hora">
             <div class="contenedor_fecha">
                 <span> SOLO x HOY</span>
-
-                <img src="{{ asset('assets/ecommerce/iconos/icono_reloj.svg') }}" alt="Logo" />
             </div>
 
             <div class="contenedor_hora">
@@ -23,40 +21,47 @@
             </div>
         </div>
 
-        <div style="position: relative; width: 100%;">
-            <!-- SLIDER -->
-            <div x-ref="slider" class="contenedor_slide">
-                @foreach ($p_elementos->imagenes as $index => $item)
-                    <div class="item_slide">
-                        <a href="{{ $item['link'] }}">
-                            <img src="{{ $item['imagen'] }}" alt="Promoción {{ $index + 1 }}" />
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-
-            <!-- CONTROL BOTONES -->
-            <div class="control_botones" x-show="totalPaginas > 1">
-                <button @click="botonRetroceder()" :disabled="paginaActual === 1" class="boton_retroceder">
-                    <i class="fa-solid fa-chevron-left"></i>
-                </button>
-                <button @click="botonSiguiente()" :disabled="paginaActual + itemsPorPagina > totalElementos"
-                    class="boton_siguiente">
-                    <i class="fa-solid fa-chevron-right"></i>
-                </button>
-            </div>
-
-            <!-- PAGINACION BOTONES -->
-            <div class="paginacion_botones" x-show="totalPaginas > 1">
-                <template x-for="itemPagina in totalPaginas" :key="itemPagina">
-                    <button @click="setPaginaActual(itemPagina)"
-                        :class="{ 'boton_activo': paginaActual === (itemPagina - 1) * itemsPorPagina + 1 }"></button>
-                </template>
+        <div class="partials_contenedor_slider_temporizador">
+            <!-- Swiper -->
+            <div class="swiper SwiperTemporizador-{{ $p_elementos->id }}">
+                <div class="swiper-wrapper">
+                    @foreach ($p_elementos->imagenes as $index => $item)
+                        <div class="swiper-slide">
+                            <a href="{{ $item['link'] }}">
+                                <img src="{{ $item['imagen'] }}" alt="}" />
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-pagination"></div>
             </div>
         </div>
     </div>
 
     <script>
+        var swiper = new Swiper(".SwiperTemporizador-{{ $p_elementos->id }}", {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            navigation: {
+                nextEl: '.SwiperTemporizador-{{ $p_elementos->id }} .swiper-button-next',
+                prevEl: '.SwiperTemporizador-{{ $p_elementos->id }} .swiper-button-prev',
+            },
+            pagination: {
+                el: '.SwiperTemporizador-{{ $p_elementos->id }} .swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 2,
+                },
+                0: {
+                    slidesPerView: 1,
+                },
+            }
+        });
+
         function dataTemporizador(fecha_fin, totalImagenes) {
             const fechaFinal = new Date(fecha_fin);
 
@@ -65,23 +70,11 @@
                 minuto: 0,
                 segundo: 0,
 
-                totalElementos: totalImagenes,
-                cantidadElementosComputadora: 2,
-                //cantidadElementosTablet: 2,
-                cantidadElementosMovil: 1,
-                itemsPorPagina: 2,
-                paginaActual: 1,
-                totalPaginas: Math.ceil(totalImagenes / 2),
-
                 initTemporizador() {
                     this.intervalo();
                     setInterval(() => {
                         this.intervalo();
                     }, 1000);
-
-                    this.anchoPantalla();
-                    window.addEventListener('resize', this.anchoPantalla.bind(this));
-
                 },
 
                 intervalo() {
@@ -98,45 +91,6 @@
                 padTwoDigits(valor) {
                     return valor.toString().padStart(2, '0').split('');
                 },
-
-                anchoPantalla() {
-                    const windowWidth = window.innerWidth;
-                    if (windowWidth > 1000) {
-                        this.itemsPorPagina = this.cantidadElementosComputadora;
-                    } /*else if (windowWidth > 700) {
-                        this.itemsPorPagina = this.cantidadElementosTablet;
-                    } */else {
-                        this.itemsPorPagina = this.cantidadElementosMovil;
-                    }
-                    this.totalPaginas = Math.ceil(this.totalElementos / this.itemsPorPagina);
-                    this.scrollPaginaActual();
-                },
-
-                botonRetroceder() {
-                    this.paginaActual = Math.max(this.paginaActual - this.itemsPorPagina, 1);
-                    this.scrollPaginaActual();
-                },
-
-                botonSiguiente() {
-                    this.paginaActual = Math.min(this.paginaActual + this.itemsPorPagina, this.totalElementos);
-                    this.scrollPaginaActual();
-                },
-
-                setPaginaActual(itemPagina) {
-                    this.paginaActual = (itemPagina - 1) * this.itemsPorPagina + 1;
-                    this.scrollPaginaActual();
-                },
-
-                scrollPaginaActual() {
-                    if (this.$refs.slider) {
-                        const scrollAmount = (this.paginaActual - 1) * (this.$refs.slider.clientWidth / this
-                            .itemsPorPagina);
-                        this.$refs.slider.scrollTo({
-                            left: scrollAmount,
-                            behavior: 'smooth'
-                        });
-                    }
-                }
             };
         }
     </script>
