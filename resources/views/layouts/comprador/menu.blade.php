00ba64bcd847c444a6838d7e656d92e64a7abe81
Mersmith <mersmith14@gmail.com>
2024-09-12 23:49:10 -0500

    menu

--- resources/views/layouts/comprador/menu.blade.php
+++ resources/views/layouts/comprador/menu.blade.php
@@ -1,11 +1,33 @@
 <div class="comprador_menu_pricipal">
-    <a href="{{ route('comprador.perfil.vista.ver') }}">Perfil</a>
-    <a href="">Mis compras</a>
-    <a href="{{ route('comprador.direccion.vista.ver') }}">Direcciones</a>
-    <a href="{{ route('comprador.reembolso.vista.ver') }}">Reembolso</a>
-    <a href="{{ route('comprador.favorito.vista.ver') }}">Favoritos</a>
+    <a href="{{ route('comprador.perfil.vista.ver') }}">
+        <span><i class="fa-solid fa-address-card"></i> Perfil</span>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
+    <a href="">
+        <span><i class="fa-solid fa-basket-shopping"></i> Mis compras</span>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
+    <a href="{{ route('comprador.direccion.vista.ver') }}">
+        <span><i class="fa-solid fa-map-location"></i> Direcciones</span>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
+    <a href="{{ route('comprador.reembolso.vista.ver') }}">
+        <span><i class="fa-solid fa-arrow-rotate-left"></i> Reembolso</span>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
+    <a href="{{ route('comprador.favorito.vista.ver') }}">
+        <span><i class="fa-solid fa-heart"></i> Favoritos</span>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
     <form method="POST" action="{{ route('logout') }}" x-data>
         @csrf
-        <a href="{{ route('logout') }}" @click.prevent="$root.submit();">Cerrar</a>
+        <a href="{{ route('logout') }}" @click.prevent="$root.submit();">
+            <span><i class="fa-solid fa-power-off"></i> Cerrar</span>
+        </a>
     </form>
 </div>
--- resources/views/livewire/comprador/reembolso/reembolso-ver-livewire.blade.php
+++ resources/views/livewire/comprador/reembolso/reembolso-ver-livewire.blade.php
@@ -1,26 +1,26 @@
 <div>
     @if (session()->has('success'))
-        <div class="comprador_alerta exito">
+        <div class="g_alerta exito">
             {{ session('success') }}
         </div>
     @endif
 
     @if (session()->has('error'))
-        <div class="comprador_alerta error">
+        <div class="g_alerta error">
             {{ session('error') }}
         </div>
     @endif
 
-    <div class="comprador_panel">
-        <div class="comprador_titulo">
+    <div class="g_panel">
+        <div class="g_titulo">
             <h2>Datos para reembolso</h2>
         </div>
 
-        <form wire:submit.prevent="updateReembolso" class="comprador_formulario">
-            <div class="bloque dos_columnas">
+        <form wire:submit.prevent="updateReembolso" class="g_formulario">
+            <div class="g_bloque dos_columnas">
                 <div class="item_formulario">
                     <label for="banco_id">Banco:</label>
-                    <select id="banco_id" wire:model="banco_id">
+                    <select wire:model="banco_id" id="banco_id" name="banco_id">
                         <option value="">Seleccionar Banco</option>
                         @foreach ($bancos as $banco)
                             <option value="{{ $banco->id }}">{{ $banco->nombre }}</option>
@@ -33,7 +33,7 @@
 
                 <div class="item_formulario">
                     <label for="tipo_cuenta_id">Tipo de Cuenta:</label>
-                    <select id="tipo_cuenta_id" wire:model="tipo_cuenta_id">
+                    <select wire:model="tipo_cuenta_id" id="tipo_cuenta_id" name="tipo_cuenta_id">
                         <option value="">Seleccionar Tipo de Cuenta</option>
                         @foreach ($tipoCuentas as $tipoCuenta)
                             <option value="{{ $tipoCuenta->id }}">{{ $tipoCuenta->nombre }}</option>
@@ -45,10 +45,10 @@
                 </div>
             </div>
 
-            <div class="bloque dos_columnas">
+            <div class="g_bloque dos_columnas">
                 <div class="item_formulario">
                     <label for="clave_actual">Cuenta Interbancaria</label>
-                    <input type="text" id="cuenta_interbancaria" wire:model="cuenta_interbancaria">
+                    <input type="text" wire:model="cuenta_interbancaria" id="clave_actual" name="clave_actual">
                     @error('cuenta_interbancaria')
                         <span class="error">{{ $message }}</span>
                     @enderror
@@ -56,15 +56,17 @@
 
                 <div class="item_formulario">
                     <label for="clave_nueva">Cuenta Bancaria</label>
-                    <input type="text" id="cuenta_bancaria" wire:model="cuenta_bancaria">
+                    <input type="text" wire:model="cuenta_bancaria" id="clave_nueva" name="clave_nueva">
                     @error('clave_nueva')
                         <span class="error">{{ $message }}</span>
                     @enderror
                 </div>
             </div>
 
-            <div class="comprador_formulario_boton">
-                <button type="submit" class="guardar">Guardar Cambios</button>
+            <div class="g_bloque tres_columnas">
+                <div class="g_formulario_boton">
+                    <button type="submit" class="guardar">Guardar Cambios</button>
+                </div>
             </div>
         </form>
     </div>
