87a3f328e112b32f9eb74c632e8f46320886171c
Mersmith <mersmith14@gmail.com>
2024-09-15 16:30:43 -0500

    avance

--- resources/views/livewire/comprador/favorito/detalle-favorito-livewire.blade.php
+++ resources/views/livewire/comprador/favorito/detalle-favorito-livewire.blade.php
@@ -7,23 +7,61 @@
     @if ($favoritos->isEmpty())
         <p>No tienes productos en tus favoritos.</p>
     @else
-        <div class="contenedor_favorito">
+        <div class="g_carrito">
             @foreach ($favoritos as $producto)
                 <div class="item_producto">
-                    <div class="contenedor_imagen">
-                        <img src="" alt="">
+                    <div class="info_producto">
+                        <div class="contenedor_imagen">
+                            <a
+                                href="{{ route('ecommerce.producto.vista.ver', ['id' => $producto->producto_id, 'slug' => $producto->producto_url]) }}">
+                                <img src="{{ $producto->imagen_url }}" alt="">
+                            </a>
+                        </div>
+
+                        <div class="contenedor_informacion">
+                            <h3 class="producto_nombre"> {{ $producto->producto_nombre }}</h3>
+                            <h4 class="marca_nombre">{{ $producto->marca_nombre }}</h4>
+
+                            @if ($producto->color_nombre)
+                                <p class="variacion">Color:
+                                    <span>{{ $producto->color_nombre }}</span>
+                                </p>
+                            @endif
+
+                            @if ($producto->talla_nombre)
+                                <p class="variacion">Talla:
+                                    <span>{{ $producto->talla_nombre }}</span>
+                                </p>
+                            @endif
+
+                            @if ($producto->precio_oferta)
+                                <p class="precio precio_oferta">S/.
+                                    {{ number_format($producto->precio_oferta, 2) }}</p>
+                            @endif
+
+                            <p class="precio precio_normal">
+                                S/. {{ number_format($producto->precio_normal, 2) }}
+                                @if ($producto->porcentaje_descuento)
+                                    <span class="descuento">
+                                        - {{ $producto->porcentaje_descuento }}%
+                                    </span>
+                                @endif
+                            </p>
+                            <p class="precio precio_antiguo">S/.
+                                {{ number_format($producto->precio_antiguo, 2) }}</p>
+                        </div>
                     </div>
 
-                    <div class="info_producto">
-                        <a
-                            href="{{ route('ecommerce.producto.vista.ver', ['id' => $producto->producto_id, 'slug' => $producto->producto_url]) }}">
-                            <span>{{ $producto->producto_id }} {{ $producto->producto_nombre }}</span>
-                        </a>
-                        <button wire:click="eliminarFavorito({{ $producto->producto_id }})">
-                            Eliminar favorito
-                        </button>
+                    <div class="controles">
+                        <div class="enlaces">
+                            <button wire:click="eliminarFavorito({{ $producto->producto_id }})">Eliminar</button>
+                        </div>
                     </div>
                 </div>
+
+                @if (!$loop->last)
+                    <div class="g_separacion"></div>
+                @endif
             @endforeach
         </div>
     @endif
